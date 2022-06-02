3d75e52853d28d3d877cbc94a91047096d52d93d
daginuz <dazuca23@gmail.com>
2022-06-01 22:06:39 -0600

    Add a Hovering Health Bar prefab
    This one can be attached to hover over the player and enemies. It's
    configured to face in the direction of the camera.
    
    The previous Health Bar prefab is maintained and intended to be used as
    a fixed HUD health bar.

---
+++ Assets/Scripts/BillboardController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Apparently, in Unity a billboard is an object that always faces the camera.
+public class BillboardController : MonoBehaviour
+{
+    public Transform camera;
+
+    // This is called last in the update process. With this we ensure that we
+    // adjust the billboard to face the camera's most recent position for this
+    // frame.
+    void LateUpdate()
+    {
+        //transform.LookAt(camera.position);
+        transform.LookAt(transform.position + camera.forward);
+    }
+}
