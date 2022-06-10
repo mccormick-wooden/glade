7621448d293d04a32b51eef3772ce0f6747e0565
Eric Gilligan <gilligan.eric@gmail.com>
2022-06-09 22:48:28 -0400

    Adds min dist for enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,9 @@ public class Enemy : BaseDamageable
     private Rigidbody rigidBody;
     public Transform Player;
 
+    [SerializeField]
+    private float minDistanceFromPlayer = 0F;
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -24,7 +27,10 @@ public class Enemy : BaseDamageable
     {
         transform.LookAt(Player);
 
-        transform.position += transform.forward * 5 * Time.deltaTime;
+        if (Vector3.Magnitude(Player.transform.position - transform.position) > minDistanceFromPlayer)
+        {
+            transform.position += transform.forward * 5 * Time.deltaTime;
+        }
     }
 
     // Update is called once per frame
