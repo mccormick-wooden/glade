341fb4e4cb032d4686640bbe0ce738f00fbf5a6d
Thomas Lagrange <tplagrange@gmail.com>
2022-06-14 23:35:22 -0400

    [WIP] Working Root Motion 3rd Person Character Controller

--- Assets/Scripts/Movement/WorldSpaceMovement.cs
+++ Assets/Scripts/Movement/WorldSpaceMovement.cs
@@ -9,18 +9,28 @@ using Vector3 = UnityEngine.Vector3;
 public class WorldSpaceMovement : MonoBehaviour, WorldSpaceControls.IWorldSpaceActionMapActions
 {
     [SerializeField] private WorldSpaceControls worldSpaceControls;
-
+    [SerializeField] private Animator animator;
+        
     public CharacterController controller;
     public Transform camera;
 
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
-
+    
+    private bool useAnimator;
     private Vector2 move;
+    
+    private static readonly int AnimatorMovementParameter = Animator.StringToHash("Input");
+    private static readonly int AnimatorIsMovingParameter = Animator.StringToHash("IsMoving");
 
     private void Awake()
     {
+        if (animator != null)
+        {
+            Debug.Log("Using root motion");
+            useAnimator = true;
+        }
         
         worldSpaceControls = new WorldSpaceControls();
         worldSpaceControls.WorldSpaceActionMap.SetCallbacks(this);
@@ -28,11 +38,54 @@ public class WorldSpaceMovement : MonoBehaviour, WorldSpaceControls.IWorldSpaceA
 
     private void Update()
     {
+        // Better root detection
+        if (useAnimator) MoveCharacterControllerRoot();
         MoveCharacterController();
     }
 
+    private void OnAnimatorMove()
+    {
+        Vector3 velocity = animator.deltaPosition;
+        velocity.y = Physics.gravity.y * Time.deltaTime;
+
+        controller.Move(velocity);
+    }
+
+    private void MoveCharacterControllerRoot()
+    {
+        Vector3 inputDirection = new Vector3();
+        
+        if (move == Vector2.zero)
+        {
+            animator.SetBool(AnimatorIsMovingParameter, false);
+            
+            // Fix this to move with camera
+            Vector3 down = Vector3.down * 9.8f;
+            controller.Move(down * Time.deltaTime);
+            return;
+        }
+
+        animator.SetBool(AnimatorIsMovingParameter, true);
+        var movement = new Vector3(move.x, 0, move.y);
+
+        /* Make Relative to Camera */
+        var targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        
+        // Look the right way
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    }
+    
     private void MoveCharacterController()
     {
+        var inputMagnitude = Mathf.Clamp01(move.magnitude);
+
+        animator.SetFloat(AnimatorMovementParameter, inputMagnitude, 0.05f, Time.deltaTime);
+        
+        move.Normalize();
+        
+        
+        
         if (move == Vector2.zero)
         {
             Vector3 down = Vector3.down * 9.8f;
@@ -47,7 +100,7 @@ public class WorldSpaceMovement : MonoBehaviour, WorldSpaceControls.IWorldSpaceA
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
         
         // Look the right way
-        transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
         
         // Move the right way
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
