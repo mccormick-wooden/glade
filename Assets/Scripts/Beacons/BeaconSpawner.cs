6fe87b063636e394a202c9b10570dc4d8a960c07
Thomas Lagrange <tplagrange@gmail.com>
2022-06-18 20:39:11 -0400

    PR fixes

--- Assets/Scripts/Beacons/BeaconSpawner.cs
+++ Assets/Scripts/Beacons/BeaconSpawner.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using UnityEngine;
 using TMPro;
 using Unity.Collections;
@@ -8,34 +7,39 @@ using Random = UnityEngine.Random;
 
 namespace Beacons
 {
-    
     public class BeaconSpawner : MonoBehaviour
     {
-        [HideInInspector]
-        public static BeaconSpawner Instance
-        {
-            get;
-            private set;
-        }
-        
-        [SerializeField] public GameObject beaconToSpawn;
+        public static BeaconSpawner Instance { get; private set; }
+
+        [SerializeField]
+        public GameObject beaconToSpawn;
 
         // The original value for this variable controls when the first beacon will drop.
         [SerializeField]
         private float spawnTimerRemaining;
+
         [SerializeField]
         private float minSpawnWaitTime;
-        [SerializeField] private float maxSpawnWaitTime;
 
-        [SerializeField, ReadOnly] private int beaconCount;
-        [SerializeField, ReadOnly] private int beaconTotal;
-        [SerializeField] private float maxConcurrentBeacons; 
-        [SerializeField] private float maxTotalBeacons;
-        
+        [SerializeField]
+        private float maxSpawnWaitTime;
+
+        [SerializeField]
+        private int beaconCount;
+
+        [SerializeField]
+        private int beaconTotal;
+
+        [SerializeField]
+        private float maxConcurrentBeacons;
+
+        [SerializeField]
+        private float maxTotalBeacons;
+
         // Provide a TMPro GUI object to output the current count
         [SerializeField]
         private TextMeshProUGUI beaconCountText;
-        
+
         private void Awake()
         {
             // Enforced Singleton
@@ -45,7 +49,6 @@ namespace Beacons
             }
             else
             {
-
                 Instance = this;
             }
         }
@@ -56,7 +59,6 @@ namespace Beacons
 
             if (spawnTimerRemaining <= 0)
             {
-                
                 Spawn();
                 spawnTimerRemaining = Random.Range(minSpawnWaitTime, maxSpawnWaitTime);
             }
@@ -64,7 +66,8 @@ namespace Beacons
 
         public void Spawn()
         {
-            if (!(beaconCount < maxConcurrentBeacons) || !(beaconTotal < maxTotalBeacons))  return;
+            if ((beaconCount >= maxConcurrentBeacons) || (beaconTotal >= maxTotalBeacons))
+                return;
 
             var transformRef = transform;
             IncrementBeaconCount();
@@ -77,8 +80,7 @@ namespace Beacons
             DecrementBeaconCount();
             Spawn();
         }
-        
-        
+
         private void DecrementBeaconCount()
         {
             SetBeaconCountText(false);
@@ -99,7 +101,8 @@ namespace Beacons
         {
             var oldCount = beaconCount;
             beaconCount = increment ? ++oldCount : --oldCount;
-            if (beaconCountText == null) return;
+            if (beaconCountText == null)
+                return;
             beaconCountText.text = $"Beacons: {(beaconCount >= 0 ? beaconCount : 0)}";
         }
     }
