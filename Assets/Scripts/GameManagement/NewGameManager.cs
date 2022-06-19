cd3fc0a239d9ba872db578b299d5ec94e2a86b34
McCormick Wooden <mwooden3@gatech.edu>
2022-06-19 15:41:14 -0500

    it's alive! inspiration: https://www.youtube.com/watch\?v\=4I0vonyqMi8

--- Assets/Scripts/GameManagement/NewGameManager.cs
+++ Assets/Scripts/GameManagement/NewGameManager.cs
@@ -1,6 +1,42 @@
-﻿public class NewGameManager : BaseSceneManager
+﻿using System;
+using UnityEngine;
+
+public class NewGameManager : BaseSceneManager
 {
     public override string ManagedSceneName => "NewGame";
 
     public override GameState ManagedState => GameState.NewGame;
+
+    private AnimationEventDispatcher animationEventDispatcher;
+
+    private bool subscribed = false;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        // I would strongly prefer to do this logic in OnEnable(), but it's pretty racy... must be doing something wrong, but shouldn't matter too much?
+        if (animationEventDispatcher == null)
+        {
+            animationEventDispatcher = GameObject.Find("CrawlText")?.GetComponent<AnimationEventDispatcher>();
+        }
+        else if (!subscribed)
+        {
+            animationEventDispatcher.OnAnimationComplete += OnAnimationComplete;
+            subscribed = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (animationEventDispatcher != null)
+            animationEventDispatcher.OnAnimationComplete -= OnAnimationComplete;
+    }
+
+    private void OnAnimationComplete(string animation)
+    {
+        Debug.Log($"{GetType().Name} received AnimationComplete for {animation}!");
+        if (animation.Equals("NewGameCrawl", StringComparison.OrdinalIgnoreCase))
+            GameManager.UpdateGameState(GameState.MainMenu);
+    }
 }
