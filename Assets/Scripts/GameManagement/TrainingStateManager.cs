5211983fa215afa0bf2e56a5746e0bf25415f162
McCormick Wooden <mwooden3@gatech.edu>
2022-07-02 23:28:02 -0500

    get training scene integrated into game management flow

---
+++ Assets/Scripts/GameManagement/TrainingStateManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainingStateManager : BaseStateManager
+{
+    /// <summary>
+    /// Controls whether the training will be skipped
+    /// </summary>
+    [Header("Scene Settings")]
+    [SerializeField]
+    private bool skipTraining = false;
+
+    //[SerializeField]
+    //private string mainExitRootName = "MainExitGame";
+
+    protected void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        if (skipTraining)
+            GameManager.UpdateGameState(GameState.Level1);
+#endif
+    }
+
+    protected override void OnSceneLoaded()
+    {
+        //Utility.AddButtonCallback(mainNewGameRootName, () => GameManager.UpdateGameState(GameState.NewGame));
+        //Utility.AddButtonCallback(mainExitRootName, () => Quitter.QuitGame());
+        //GameObject.Find(mainNewGameRootName).GetComponentInChildren<Button>().Select();
+    }
+
+    protected override void OnSceneUnloaded()
+    {
+    }
+}
