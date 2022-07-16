67612034d163aa0195130cc5ceea2e9a0fd6f6c3
lotsandlotsofrobots <lotsandlotsofrobots@noreply.users.github.com>
2022-07-16 10:43:25 -0500

    Slash trail effects on hit enemy

--- Assets/Scripts/WeaponsAndAttacks/Sword.cs
+++ Assets/Scripts/WeaponsAndAttacks/Sword.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Sword : BaseWeapon
 {
     private string[] validCollideTags = new string[] { "Enemy", "Damageable" };
+    private ParticleSystem trails;
 
     protected override void Start()
     {
         base.Start();
         TargetTags = new string[] { "Enemy", "Damageable" };  
+        trails = transform.Find("Trails").GetComponent<ParticleSystem>();
+        trails.Stop();
     }
     
     private void OnCollisionEnter(Collision collision)
@@ -18,10 +21,20 @@ public class Sword : BaseWeapon
         {
             if (TargetTags.Contains(collision.gameObject.tag))
             {
-
+                Debug.Log("hit a thing!  do trail!");
+                trails.Play();
             }
 
             Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (InUse && TargetTags.Contains(other.gameObject.tag))
+        {
+            Debug.Log("hit a thing!  do trail!");
+            trails.Play();
         }
     }
 }
