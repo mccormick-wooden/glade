8114125515c0f2d02a9b78c1149f5954123d0eac
Thomas Lagrange <tplagrange@gmail.com>
2022-07-16 00:18:08 -0400

    LockOnIndicator

--- Assets/PlayerLockOnCamera.cs
+++ Assets/PlayerLockOnCamera.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 
 public class PlayerLockOnCamera : MonoBehaviour
 {
+    public GameObject lockOnIndicatorPreFab;
+    private GameObject lockOnIndicatorInstance;
+    private float lockOnIndicatorYOffset = 0f;
+
     private CinemachineFreeLook thirdPersonCamera;
     private CinemachineVirtualCamera lockOnCamera;
-
-    private Transform lockOnTarget;
 
     private bool isLockingOn;
 
@@ -21,20 +23,67 @@ public class PlayerLockOnCamera : MonoBehaviour
         Utility.LogErrorIfNull(thirdPersonCamera, "thirdPersonCamera");
     }
 
+    private void FixedUpdate()
+    {
+        UpdateVisualizer();
+    }
+
     public void EnableLockOnCamera(Transform newLockOnTarget)
     {
-        Debug.Log("Enabling Lock On Camera");
+        if (newLockOnTarget == null)
+        {
+            return;
+        }
 
+        isLockingOn = true;
         lockOnCamera.LookAt = newLockOnTarget;
-
         lockOnCamera.enabled = true;
         thirdPersonCamera.enabled = false;
+
+        lockOnIndicatorYOffset = newLockOnTarget.localPosition.y * 2f;
+
+        EnableVisualizer();
     }
 
     public void DisableLockOnCamera()
     {
-        Debug.Log("Disabling Lock On Camera");
+        isLockingOn = false;
         thirdPersonCamera.enabled = true;
         lockOnCamera.enabled = false;
+
+        lockOnIndicatorYOffset = 0f;
+
+        DisableVisualizer();
+    }
+
+    private void EnableVisualizer()
+    {
+        if (lockOnIndicatorInstance == null)
+        {
+            lockOnIndicatorInstance =
+                Instantiate(lockOnIndicatorPreFab, lockOnCamera.LookAt.position, Quaternion.identity);
+        }
+        else
+        {
+            lockOnIndicatorInstance.SetActive(true);
+        }
+    }
+
+    private void DisableVisualizer()
+    {
+        lockOnIndicatorInstance.SetActive(false);
+    }
+
+    private void UpdateVisualizer()
+    {
+        if (!isLockingOn)
+        {
+            return;
+        }
+
+        var target = lockOnCamera.LookAt.transform.position;
+        var newPosition = new Vector3(target.x, target.y + lockOnIndicatorYOffset, target.z);
+
+        lockOnIndicatorInstance.transform.position = newPosition;
     }
 }
