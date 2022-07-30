e7f1efdfd8673c2a81d3ce9e1b0fd5b7169f7acb
lotsandlotsofrobots <lotsandlotsofrobots@users.noreply.com>
2022-07-30 14:18:40 -0500

    mushroom boi death

--- Assets/Scripts/WeaponsAndAttacks/MushroomExplosionWeapon.cs
+++ Assets/Scripts/WeaponsAndAttacks/MushroomExplosionWeapon.cs
@@ -1,11 +1,20 @@
 using System;
 using Assets.Scripts.Abstract;
+using UnityEngine;
 
 public class MushroomExplosionWeapon : BaseWeapon
 {
     private string[] validCollideTags = new string[] { "Player" };
     DateTime creationTime;
 
+    [SerializeField]
+    AudioClip explodeSound = null;
+
+    [SerializeField]
+    float explodePitch;
+
+    [SerializeField]
+    float explodeVolume;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -14,6 +23,8 @@ public class MushroomExplosionWeapon : BaseWeapon
         TargetTags = new string[] { "Player" };
         InUse = true;
         creationTime = DateTime.Now;
+
+        EventManager.TriggerEvent<MonsterDieEvent, Vector3, AudioClip, float, float>(transform.position, explodeSound, explodePitch, explodeVolume);
     }
 
     // Update is called once per frame
