ef10f9b7d3ec70ea58b72ef80e2f7ff19273df1a
Eric Gilligan <gilligan.eric@gmail.com>
2022-07-31 21:53:16 -0400

    add crystals on rails

---
+++ Assets/Scripts/Crystal/CrystalSpawnPoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crystal
+{
+    // Class used as targets for first crystal to spawn on upon beacon land
+     public class CrystalSpawnPoint : MonoBehaviour
+    {
+        public uint spawnOrder;
+    }
+}
